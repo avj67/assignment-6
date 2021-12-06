a3fdc7310fe6080712226059d7630283e4761c01
J <93539076+avj67@users.noreply.github.com>
2021-12-06 09:36:17 +0530

    Initial commit

---
+++ question 1.py
@@ -0,0 +1,15 @@
+#Given two dictionaries, d1 and d2, where values are numbers, add values from both the dictionaries with
+#common keys. Form a new dictionary with such key, value pairs.
+
+d1= {'a':10, 'b':20,'c':30}
+d2= {'b':20 , 'c':30, 'd':40}
+d3={}
+            key=i
+            value = d1[i] +d2[j]
+            d3={key:value}
+            d4.update(d3)
+print(d4)
+d4={}
+for i in d1:
+    for j in d2:
+        if i==j:
