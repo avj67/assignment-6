4bbf0f88c35830aaca1eea2b2f99ecda137fd981
J <93539076+avj67@users.noreply.github.com>
2021-12-06 10:29:49 +0530

    question 2
    Signed-off-by: J <93539076+avj67@users.noreply.github.com>

---
+++ venv/question 2.py
@@ -0,0 +1,38 @@
+# creating a dictionary with roll number, name, marks of n students in a class and display names of students with
+# marks above 75
+
+# creating the dicitonary
+marks={}
+temp_marks={}
+num = int(input("How many students do you wish to add to the database: "))
+for i in range (num):
+    user_li=[]
+    print()
+
+    #inputting required data
+    name = input("Enter the student's name: ")
+    roll = int(input("Enter the student's roll number: "))
+    student_marks = int(input("Enter the student's marks: "))
+
+    #creating list
+    user_li.append(name)
+    user_li.append(student_marks)
+
+    # adding values to a temp dictionary
+    temp_marks={roll:user_li}
+
+    # updating main dictionary with temp dicitionary's values
+    marks.update(temp_marks)
+
+print("Dictionary of students: ",marks)
+
+# displaying names above 75
+
+
+print("Students with marks above 75: ")
+for key in marks:
+    if marks[key][1] > 75:
+         print(marks[key][0], end= ", ")
+
+    else:
+         continue
