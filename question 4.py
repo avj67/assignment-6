67110f2246e8e4b5056200389aa41b32ffd4dad0
J <93539076+avj67@users.noreply.github.com>
2021-12-17 21:15:22 +0530

    fixed question 4
    Signed-off-by: J <93539076+avj67@users.noreply.github.com>

--- question 4.py
+++ question 4.py
@@ -15,54 +15,57 @@ print(
 =======================================
 ''')
 print()
-while loop_check > 0:
-    option = int(input("Welcome please choose an option: "))
+while True :
+    if loop_check >0 :
+        option = int(input("Welcome please choose an option: "))
 
-    # searching for an item
-    if option == 1:
-        print()
-        item_search = input("Enter item name please: ")
+        # searching for an item
+        if option == 1:
+            print()
+            item_search = input("Enter item name please: ")
 
-        if item_search in shop_list:
-            for i in shop_list:
-                if item_search == i:
-                    print("Item: ", i)
-                    print("Stock: ", shop_list[i][0])
-                    print("Price: ", shop_list[i][1])
+            if item_search in shop_list:
+                for i in shop_list:
+                    if item_search == i:
+                        print("Item: ", i)
+                        print("Stock: ", shop_list[i][0])
+                        print("Price: ", shop_list[i][1])
+
+            else:
+                print("Item not found")
+
+        #  renaming an item
+        if option == 2:
+            print()
+            item_rename = input("Enter item to be renamed please: ")
+            new_name = input("Enter the item's new name please: ")
+
+
+            if item_rename in shop_list:
+                # dictionary keys are uneditable so what we do is we make a new key with same values and delete old one
+                temp_value = shop_list[item_rename]
+                temp_dict={new_name:temp_value} #creating a temporary dictionary with new key name and old values
+                shop_list.pop(item_rename) # deletes key with string value matching input from user
+                shop_list.update(temp_dict) # adds new name with same stock and price
+                print("Item has been renamed")
+            else:
+                print(item_rename," does not exist in database")
+
+        # 3rd thingy
 
         else:
-            print("Item not found")
+            x=1
 
-    #  renaming an item
-    if option == 2:
+
         print()
-        item_rename = input("Enter item to be renamed please: ")
-        new_name = input("Enter the item's new name please: ")
-
-
-        if item_rename in shop_list:
-            # dictionary keys are uneditable so what we do is we make a new key with same values and delete old one
-            temp_value = shop_list[item_rename]
-            temp_dict={new_name:temp_value} #creating a temporary dictionary with new key name and old values
-            shop_list.del(item_rename) # deletes key with string value matching input from user
-            shop_list.extend(temp_dict) # adds new name with same stock and price
-            print("Item has been renamed")
+        continuity = input("Do you wish to continue? y/n (anything other than y shall be considered as program termination ")
+        if continuity == "y":
+            loop_check = 1
         else:
-            print(item_rename," does not exist in database")
-
-    # 3rd thingy
-
+            loop_check = 0
     else:
-        continue
-
-    print()
-    continuity = input("Do you wish to continue? y/n (anything other than y shall be considered as program termination ")
-    if continuity == "y":
-        loop_check = 1
-    else:
-        loop_check = 0
-
+        break
 print()
-print("goodbye")
+print("Thank you for using our services!")
 
 
